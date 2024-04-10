c2e9009127aca7b031fecc9d666b7af4a6b930c2
mike242424 <mikebyers24@gmail.com>
2024-04-10 11:10:22 -0400

    added twrnc library

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { Text } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'twrnc';
 
 export default function App() {
   return (
